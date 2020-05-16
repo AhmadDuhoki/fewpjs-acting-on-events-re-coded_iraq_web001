c29b15b5d76137c607264ef7cad0f2039bf0e8b3
Ahmad A. Duhoki <ahmad.a.duhoki99@gmail.com>
2020-05-16 07:01:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,13 +10,19 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
-  let rightNumbers = dodger.style.right.replace("px", "");
-  let right = parseInt(rightNumbers, 10);
-     
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+
   if (left > 0) {
-    dodger.style.right = `${right + 1}px`;
+    dodger.style.left = `${left + 1}px`;
   }
-}
+   if (left > 360) {
+    dodger.style.left = `${left - 1}px`;
+  }
+    if (left < 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+
 
 document.addEventListener("keydown", function(e) {
   if (e.key === "ArrowLeft") {
