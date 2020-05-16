1e92476b13dad0e828741a7ae27d884c1e0bc561
Ahmad A. Duhoki <ahmad.a.duhoki99@gmail.com>
2020-05-16 06:56:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-    let dodger = document.getElementById("dodger");
+let dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+     
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+     
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
