7bd0efc71204f8f241943b96822d7748b9143d11
Ahmad A. Duhoki <ahmad.a.duhoki99@gmail.com>
2020-05-16 06:58:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function moveDodgerRight() {
   let right = parseInt(rightNumbers, 10);
      
   if (left > 0) {
-    dodger.style.left = `${right + 1}px`;
+    dodger.style.right = `${right + 1}px`;
   }
 }
 
